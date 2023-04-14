e3f1c9d7ce83f8720cf595261b41c5d0814b4317
yennampavan <118048978+yennampavan@users.noreply.github.com>
2023-04-14 12:03:07 +0530

    version 1

---
+++ login.js
@@ -0,0 +1,1 @@
+document.getElementById("headingElement").textContent="Nani";
---
+++ mrec.js
@@ -0,0 +1,8 @@
+var items=document.getElementById("items").value; 
+
+function sun(){
+    document.getElementById("sectioncucumber").style.backgroundColor="#FEF8DD";
+}
+function moon(){
+    document.getElementById("sectioncucumber").style.backgroundColor="#ACDDDE";
+}
